1db5ba58590c4995c00902610a527b9b7b6b45a3
xmanatsf <121685805+xmanatsf@users.noreply.github.com>
2023-01-03 19:27:21 -0800

    Update US_Stock_Snap_Scatter.py

--- US_Stock_Snap_Scatter.py
+++ US_Stock_Snap_Scatter.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import statsmodels.api as sm
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 
