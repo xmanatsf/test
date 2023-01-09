e7d69259a57271a4d966c26cd73308c571b5f405
xmanatsf <121685805+xmanatsf@users.noreply.github.com>
2023-01-08 19:50:17 -0800

    Update US_Stock_Snap_Scatter.py

--- US_Stock_Snap_Scatter.py
+++ US_Stock_Snap_Scatter.py
@@ -36,7 +36,7 @@ app.layout = html.Div([
         value = None, style={'width': '25%', 'display': 'inline-block'}),
      
      dcc.Dropdown( id = 'dropdown4',
-        options = ['20230103', '20221123', '20221001'],
+        options = ['20230107','20230103', '20221123', '20221001'],
         value = None, style={'width': '25%', 'display': 'inline-block'})
      
      ]),## drop down select y variable
